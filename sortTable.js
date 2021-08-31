d9ac488e4e77ecd3861a262077aec9b72333a6c1
cyber-ikegami <ikegami@cyberbeing.tech>
2021-08-31 17:49:33 +0900

    0831進捗状況

--- sortTable.js
+++ sortTable.js
@@ -1,5 +1,26 @@
-function loadTable(){
-	const excelData = document.getElementById("excelData").value;
+// 入力必須チェック　(後で実装)
+// function inputRequiredCheck() {
+// 	const excelData = document.getElementById("excelData");
+// 	const load = document.getElementById("load");
+// 	let flag = 0;
+
+//     if(excelData.value == ""){
+//         flag = 1;
+//     }
+
+//     if(flag){
+//         load.disabled = true;
+//     } else {
+//         load.disabled = false;
+//     }
+// }
+
+function getHeaderData(){
+	const excelData = document.getElementById('excelData').value;
+    const recordList = excelData.split(/\n/g);
+    let HeaderList = [];
+    HeaderList[0] = recordList[0].split(/[,\t]/g);
     
     
+
 }
