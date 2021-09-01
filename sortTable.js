a3169b8ca79e0d0be589fb51dbaa34b02bfdf2c9
cyber-ikegami <ikegami@cyberbeing.tech>
2021-09-01 17:25:51 +0900

    0901進捗状況_2

--- sortTable.js
+++ sortTable.js
@@ -1,55 +1,36 @@
-// 入力必須チェック　(後で実装)
+// 入力必須チェック
 function inputRequiredCheck() {
 	const excelData = document.getElementById("excelData");
 	const load = document.getElementById("load");
-	let flag = 0;
 
     if(excelData.value == ""){
-        flag = 1;
-    }
-
-    if(flag){
         load.disabled = true;
     } else {
         load.disabled = false;
     }
 }
 
+// 読み込みボタン押下時動作
+// ヘッダーの値を取得し、ソート順コンボボックスに出力
 function getHeaderData(){
-	const excelData = document.getElementById('excelData').value;
-    const recordList = excelData.split(/\n/g);
-    let HeaderList = [];
-
-    for(let i = 0; i < recordList.length; i++){
-    HeaderList[i] = recordList[0].split(/[,\t]/g);
-    }
-    // let menuValue = ""; 
+    const sortOrder = document.getElementById('sortOrder');
+    sortOrder.disabled = false;
     
-    // メモ：連想配列を作りたい
-    // for(let i = 0; i < HeaderList.length; i++){
-    //     HeaderList[i] = recordList[0].split(/[,\t]/g);
-    //     menuValue = menuValue + '\'' + HeaderList[i] + '\'' + ':' + '\'' + i + '\''
-    // メモ：i + 1をどうにかする
-    //     if(i + 1 > HeaderList.length){
-    //         menuValue = menuValue + ',';
-    //     } else {
-    //         break;
+    // メモ：コンボボックスのリセットがしたい
+    // while(sortOrder.lastChild){
+    //     if(!document.getElementById('null')){
+    //         sortOrder.removeChild(sortOrder.lastChild);
     //     }
     // }
 
-    // 確認用
-    alert(HeaderList)
-
-    // 参考サイトコピー（メモ）
-    // let menu = {menuValue};
-    // let id = document.getElementById('sortOrder');
-
-    // // メニュー項目のセット
-    // for (let i in menu) 
-    // {
-    //     let element = document.createElement('option');
-    //     element.setAttribute('value', i);
-    //     element.innerHTML = menu[i];
-    //     id.appendChild( element );
-    // }
+	const excelData = document.getElementById('excelData').value;
+    const recordList = excelData.split(/\n/g);
+    let headerList = recordList[0].split(/[,\t]/g);
+    
+    for(let i = 0; i < headerList.length; i++){
+        let option = document.createElement("option");
+        option.text = headerList[i];
+        option.value = i;
+        sortOrder.appendChild(option);
+    }
 }
