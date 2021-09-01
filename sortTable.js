b0df3c878aaf43755b9adef61b1063e67c23a949
cyber-ikegami <ikegami@cyberbeing.tech>
2021-09-01 13:30:07 +0900

    0901 進捗状況

--- sortTable.js
+++ sortTable.js
@@ -1,26 +1,55 @@
 // 入力必須チェック　(後で実装)
-// function inputRequiredCheck() {
-// 	const excelData = document.getElementById("excelData");
-// 	const load = document.getElementById("load");
-// 	let flag = 0;
+function inputRequiredCheck() {
+	const excelData = document.getElementById("excelData");
+	const load = document.getElementById("load");
+	let flag = 0;
 
-//     if(excelData.value == ""){
-//         flag = 1;
-//     }
+    if(excelData.value == ""){
+        flag = 1;
+    }
 
-//     if(flag){
-//         load.disabled = true;
-//     } else {
-//         load.disabled = false;
-//     }
-// }
+    if(flag){
+        load.disabled = true;
+    } else {
+        load.disabled = false;
+    }
+}
 
 function getHeaderData(){
 	const excelData = document.getElementById('excelData').value;
     const recordList = excelData.split(/\n/g);
     let HeaderList = [];
-    HeaderList[0] = recordList[0].split(/[,\t]/g);
-    
-    
 
+    for(let i = 0; i < recordList.length; i++){
+    HeaderList[i] = recordList[0].split(/[,\t]/g);
+    }
+    // let menuValue = ""; 
+    
+    // メモ：連想配列を作りたい
+    // for(let i = 0; i < HeaderList.length; i++){
+    //     HeaderList[i] = recordList[0].split(/[,\t]/g);
+    //     menuValue = menuValue + '\'' + HeaderList[i] + '\'' + ':' + '\'' + i + '\''
+    // メモ：i + 1をどうにかする
+    //     if(i + 1 > HeaderList.length){
+    //         menuValue = menuValue + ',';
+    //     } else {
+    //         break;
+    //     }
+    // }
+
+    // 確認用
+    alert(HeaderList)
+
+    // 参考サイトコピー（メモ）
+    // let menu = {menuValue};
+    // let id = document.getElementById('sortOrder');
+
+    // // メニュー項目のセット
+    // for (let i in menu) 
+    // {
+    //     let element = document.createElement('option');
+    //     element.setAttribute('value', i);
+    //     element.innerHTML = menu[i];
+    //     id.appendChild( element );
+    // }
 }
